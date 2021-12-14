bd709464df9b55db02bf4fd27ff2101a1d05499b
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2021-12-14 09:07:21 +0000

    Retouche

--- phpcrud/update.php
+++ phpcrud/update.php
@@ -38,7 +38,8 @@ if(isset($_POST['submit'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/enregistrement.css">
-    <title>S'inscrire</title>
+    <link rel="stylesheet" href="css/update.css">
+    <title>Mise à jour des informations</title>
 </head>
 <body>
     <div class="container">
